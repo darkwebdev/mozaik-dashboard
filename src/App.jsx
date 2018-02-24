e10b05f68a35e9410bc86f3c7da5310cb6a2985d
Timur Manyanov <gtibalt@gmail.com>
2018-02-25 00:19:03 +0100

    Add time, weather, value extensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import React   from 'react';
-import Mozaik  from 'mozaik/browser';
-import github  from 'mozaik-ext-github';
-import travis  from 'mozaik-ext-travis';
-import time    from 'mozaik-ext-time';
-
+import React from 'react';
+import Mozaik from 'mozaik/browser';
+import time from 'mozaik-ext-time';
+import weather from 'mozaik-ext-weather';
+import value from 'mozaik-ext-value';
 
 const MozaikComponent = Mozaik.Component.Mozaik;
-const ConfigActions   = Mozaik.Actions.Config;
-
+const ConfigActions = Mozaik.Actions.Config;
 
 Mozaik.Registry.addExtensions({
-    github,
-    travis,
-    time,
+  time,
+  weather,
+  value
 });
 
 React.render(<MozaikComponent/>, document.getElementById('mozaik'));
